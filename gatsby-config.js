8bcc018e871ecb31eb322f2ed01648b12d136371
erasebegin <chris.james.haupt@gmail.com>
2020-02-03 19:23:14 +0000

    added contentful plugin plus env variables

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,28 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
     title: `Otherworld Ink`,
     description: `The canvas becomes a gateway into parallel worlds of wonder`,
-    author: `@gatsbyjs`,
+    author: `@gatsbyjs`
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+      }
+    },
+    "gatsby-plugin-react-helmet",
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+        path: `${__dirname}/src/images`
+      }
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
@@ -24,11 +35,11 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/otherworld-logo.svg`, // This path is relative to the root of the site.
-      },
-    },
+        icon: `src/images/otherworld-logo.svg` // This path is relative to the root of the site.
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-  ],
-}
+  ]
+};
