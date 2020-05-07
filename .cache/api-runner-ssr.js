e7bbdbaada4fd0c2fc586389c2b770b523fcd306
erasebegin <chris.james.haupt@gmail.com>
2020-05-07 18:29:31 +0100

    fix paypal api key error attempt 2

--- .cache/api-runner-ssr.js
+++ .cache/api-runner-ssr.js
@@ -5,9 +5,6 @@ var plugins = [{
       plugin: require('/home/chris/repos/otherworld-ink/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
       options: {"plugins":[],"name":"gatsby-starter-default","short_name":"starter","start_url":"/","background_color":"#663399","theme_color":"#663399","display":"minimal-ui","icon":"src/images/otherworld-logo.svg"},
     },{
-      plugin: require('/home/chris/repos/otherworld-ink/node_modules/gatsby-plugin-snipcart/gatsby-ssr'),
-      options: {"plugins":[],"autopop":true},
-    },{
       plugin: require('/home/chris/repos/otherworld-ink/gatsby-ssr'),
       options: {"plugins":[]},
     }]
