ed1be21fff0281242496d51a989e0c333e0d6b8d
erasebegin <chris.james.haupt@gmail.com>
2020-09-23 17:17:16 +0100

    attempting to fix header image height issue from caused by recent changes

--- src/components/globals/Background.js
+++ src/components/globals/Background.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import logo from "../../images/otherworld-logo.svg";
 import loadable from "@loadable/component";
 
 // gatsby-background-image was causing HTML build error since it tried to access the window object which is not available at build time
@@ -12,6 +12,7 @@ export default function Background({ img, styleClass, title, children }) {
       <h1 className="title text-white text-uppercase text-center display-4 font-weight-bold">
         {title}
       </h1>
+      <img src={logo} className="main-logo" alt="main logo"></img>
       {children}
     </BackgroundImage>
   );
@@ -19,5 +20,5 @@ export default function Background({ img, styleClass, title, children }) {
 
 Background.defaultProps = {
   title: "default title",
-  styleClass: "default-background",
+  styleClass: "default-background"
 };
